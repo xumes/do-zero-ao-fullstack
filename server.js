ddf268de78c1ad61c0d77892cca192b9bace8643
Reginaldo Santos <reginaldo.santos@proposify.com>
2020-12-21 16:05:06 -0400

    Creating the server

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3080;
+
+app.get('/', (req, res) => {
+    res.json({
+        'success': true
+    });
+});
+
+app.get('/portfolio', (req, res) => {
+    // acess db
+    const data = [ {
+        id: 1,
+        name: 'My first project',
+        createdAt: '2020-12-01'
+    },
+    {
+        id: 2,
+        name: 'My second project',
+        createdAt: '2020-01-06'
+    },
+    {
+        id: 3,
+        name: 'My other project',
+        createdAt: '2021-01-25'
+    },
+];
+
+    res.json({
+        success: true,
+        data: data
+    });
+});
+
+app.listen(PORT);
