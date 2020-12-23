ed3403ad92b422b28cb5d0f793511f0c50159a08
Reginaldo Santos <reginaldo.santos@proposify.com>
2020-12-23 18:34:29 -0400

    Delete portfolio from MongoDB

--- backend/routes/portfolio.js
+++ backend/routes/portfolio.js
@@ -1,22 +1,8 @@
 const router = require('express').Router();
+const { update } = require('../models/Portfolio');
 const Portfolio = require('../models/Portfolio');
 
-router.get('/', async (req, res) => {
-    try{
-        const portfolio = await Portfolio.find()
-
-        res.json({
-            success: true,
-            data: portfolio
-        })
-    }catch(err){
-        res.json({
-            success:false,
-            message: err
-        })
-    }
-});
-
+// Create
 router.post('/', async (req, res) => {
     const portfolio = new Portfolio({
         title: req.body.title,
@@ -35,6 +21,87 @@ router.post('/', async (req, res) => {
             message: err
         })
     }
+});
+
+// Read
+router.get('/', async (req, res) => {
+    try{
+        const portfolio = await Portfolio.find()
+
+        res.json({
+            success: true,
+            data: portfolio
+        })
+    }catch(err){
+        res.json({
+            success:false,
+            message: err
+        })
+    }
+});
+
+router.get('/:slug', async (req, res) => {
+    try{
+        const portfolio = await Portfolio.findOne({
+            slug: req.params.slug
+        })
+
+        res.json({
+            success: true,
+            data: portfolio
+        })
+    }catch(err){
+        res.json({
+            success: false,
+            message: err
+        })
+    }
+});
+
+//Upate
+router.patch('/:slug', async (req, res) => {
+    try{
+        const updatedPortfolio = await Portfolio.updateOne({
+            slug: req.params.slug
+        },
+        {
+            $set: {
+                title: req.body.title,
+                description: req.body.description
+            }
+        })
+
+        res.json({
+            success: true,
+            updated: updatedPortfolio.nModified
+        })
+    } catch(err){
+        res.json({
+            success: false,
+            message: err
+        })
+    }
 })
+
+//Delete
+router.delete('/:slug', async (req, res) => {
+    try{
+        const deletedPortfolio = await Portfolio.deleteOne({
+            slug: req.params.slug
+        });
+
+        res.json({
+            success: true,
+            deleted: deletedPortfolio.deletedCount
+        })
+    } catch(err) {
+        res.json({
+            success: false,
+            message: err
+        })
+    }
+
+})
+
 
 module.exports = router
