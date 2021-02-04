5829471c60c4f552dc19473a53d717fdda7b1178
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-02-04 18:21:57 -0400

    execution scripts plus proxy

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
+const leDados = ( ) => {
+  fetch('/api')
+  .then(resultado => {
+    console.log("dados do backend", resultado)
+  })
+}
+
 
 function App() {
   return (
     <div>
+      {leDados()}
       Bem vindo ao frontend
     </div>
   );
