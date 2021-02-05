ce51d2b072ecb76b3eeb5f695aa00ad44c559776
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-02-04 21:55:22 -0400

    Local styles

---
+++ frontend/src/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components'
+
+export const About = styled.div`
+    min-height: 90vh;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 5rem 10rem;
+    color: white;
+`;
+
+export const Description = styled.div`
+    flex:1;
+    padding-right: 5rem;
+    z-index: 2;
+    h2 {
+        font-weight: lighter;
+        color: yellow;
+    }
+`;
+
+export const Image = styled.div`
+    z-index: 2;
+    flex: 1;
+    overflow: hidden;
+    img {
+        width: 100%;
+        height: 75;
+        object-fit: cover;
+    }
+`;
