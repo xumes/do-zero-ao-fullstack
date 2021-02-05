78051db8b61417039f7a0a0af74e3291a42afbeb
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-02-04 21:55:50 -0400

    About Us page

---
+++ frontend/src/pages/AboutUs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import AboutSection from '../components/AboutSection'
+import ServicesSection from '../components/ServicesSection'
+
+const AboutUs = () => {
+    return(
+        <div>
+            <AboutSection />
+            <ServicesSection />
+        </div>
+    )
+}
+
+export default AboutUs
