ffa850754f414f738583da31443cdc300f08b497
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-02-14 18:57:50 -0400

    Contact Me final version

---
+++ frontend/src/components/ContactForm.js
@@ -0,0 +1,66 @@
+import React from 'react'
+
+import styled from 'styled-components'
+
+const ContactForm = () => {
+    return(
+        <Form id="contact-form" method="post">
+            <Input>
+                <label>Name:</label>
+                <input type="text"/>
+            </Input>
+            <Input>
+                <label>Email:</label>
+                <input type="email" />
+            </Input>
+            <Input>
+                <label>Message:</label>
+                <textarea rows='5' />
+            </Input>
+            <button type="submit">Submit</button>
+        </Form>
+    )
+}
+
+const Input = styled.div`
+    display: flex;
+    flex-direction: column;
+    min-width: 60vh;
+    label {
+        color: white;
+        padding-top: 1rem;
+    }
+    input {
+        border: 2px solid yellow;
+        padding: 12px 20px;
+        border-radius: 10%;
+        width: 100%;
+        height: 100%;
+    }
+    input:focus {
+        background-color: #416CD5;
+        color: yellow;
+    }
+    textarea {
+        width: 100%;
+        height: 150%;
+        padding: 12px 20px;
+        box-align: border-box;
+        border: 2px solid #ccc;
+        border-radius: 4px;
+        background-color: #f8f8f8;
+        resize: none;
+    }
+    textarea:focus {
+        background-color: #416CD5;
+        color: yellow;
+    }
+`;
+
+const Form = styled.form`
+    button{
+        margin-top: 1rem;
+    }
+`;
+
+export default ContactForm
--- frontend/src/pages/ContactMe.js
+++ frontend/src/pages/ContactMe.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 import styled from 'styled-components'
 
+import SocialNetworkSection from '../components/SocialNetworkSection'
+import ContactForm from '../components/ContactForm'
+
+
 const ContactMe = () => {
     return (
         <ContactStyled>
@@ -9,36 +13,8 @@ const ContactMe = () => {
                 <h2>Get in touch</h2>
             </Title>
             <Areas>
-                <div id="form">
-                    <form id="contact-form" method="post">
-                        Name: <input type="text"/>
-                        Email: <input type="email" />
-                        Message: <textarea rows='5' />
-                        <button type="submit">Submit</button>
-                    </form>
-                </div>
-                <Network>
-                    <Social href="http://github.com/xumes" target='_blank'>
-                        icon
-                        <h3>github.com/xumes</h3>
-                    </Social>
-                    <Social>
-                        icon
-                        <h3>LinkedIn</h3>
-                    </Social>
-                    <Social>
-                        icon
-                        <h3>Youtube</h3>
-                    </Social>
-                    <Social>
-                        icon
-                        <h3>Udemy</h3>
-                    </Social>
-                    <Social>
-                        icon
-                        <h3>Telegram</h3>
-                    </Social>
-                </Network>
+                <ContactForm />
+                <SocialNetworkSection />
             </Areas>
         </ContactStyled>
     )
@@ -62,22 +38,5 @@ const Areas = styled.div`
     align-items: center;
 `;
 
-const Network = styled.div`
-    padding: 1rem 5rem;
-`;
-
-const Social = styled.a`
-    display: flex;
-    flex-direction: row;
-    justify-content: flex-start;
-    align-items: center;
-    border-radius: 15px 50px;
-    padding-left: 2rem;
-    text-decoration: none;
-    &:hover{
-        background: yellow;
-        cursor: pointer;
-    }
-`;
 
 export default ContactMe
