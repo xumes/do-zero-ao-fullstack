b16aa01413c305be79afc4ff4346735231d00e3a
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-02-14 18:21:22 -0400

    Social Media Network

--- frontend/src/pages/ContactMe.js
+++ frontend/src/pages/ContactMe.js
@@ -1,10 +1,14 @@
 import React from 'react'
 
+import styled from 'styled-components'
+
 const ContactMe = () => {
     return (
-        <div>
-            <div>Get in touch</div>
-            <div id="areas">
+        <ContactStyled>
+            <Title>
+                <h2>Get in touch</h2>
+            </Title>
+            <Areas>
                 <div id="form">
                     <form id="contact-form" method="post">
                         Name: <input type="text"/>
@@ -13,31 +17,67 @@ const ContactMe = () => {
                         <button type="submit">Submit</button>
                     </form>
                 </div>
-                <div id="network">
-                    <div className="social">
+                <Network>
+                    <Social href="http://github.com/xumes" target='_blank'>
                         icon
                         <h3>github.com/xumes</h3>
-                    </div>
-                    <div className="social">
+                    </Social>
+                    <Social>
                         icon
                         <h3>LinkedIn</h3>
-                    </div>
-                    <div className="social">
+                    </Social>
+                    <Social>
                         icon
                         <h3>Youtube</h3>
-                    </div>
-                    <div className="social">
+                    </Social>
+                    <Social>
                         icon
                         <h3>Udemy</h3>
-                    </div>
-                    <div className="social">
+                    </Social>
+                    <Social>
                         icon
                         <h3>Telegram</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
+                    </Social>
+                </Network>
+            </Areas>
+        </ContactStyled>
     )
 }
+
+const ContactStyled = styled.div`
+    padding: 1rem 10rem;
+    color: #353535;
+    min-height: 90vh;
+`;
+
+const Title = styled.div`
+    margin-bottom: 4rem;
+    h2 {
+        color: white;
+    }
+`;
+
+const Areas = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const Network = styled.div`
+    padding: 1rem 5rem;
+`;
+
+const Social = styled.a`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    border-radius: 15px 50px;
+    padding-left: 2rem;
+    text-decoration: none;
+    &:hover{
+        background: yellow;
+        cursor: pointer;
+    }
+`;
 
 export default ContactMe
