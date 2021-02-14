868ffdbf149b67253e1bfb1ec538e1fd973e5dbe
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-02-14 19:33:34 -0400

    Express serving static files

--- server.js
+++ server.js
@@ -8,13 +8,20 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.json({
-        'success': true
-    });
-});
-
 app.use('/api', api);
+
+if ( process.env.NODE_ENV === 'production' ) {
+    // Express vai entregar os assets de produção
+    // Como por exemplo: main.js ou o main.css
+    app.use(express.static('frontend/buid'));
+
+
+    // Express vai entregar o index.html, se não reconhecer a rota
+    const path = require('path')
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    })
+}
 
 const PORT = process.env.PORT;
 app.listen(PORT);
