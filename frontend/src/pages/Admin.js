aaf40c10ecb1d004123b438a4a3a81b9f32ba692
Reginaldo Santos <reginaldo.santos@proposify.com>
2021-03-10 17:07:11 -0400

    security

--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -5,7 +5,34 @@ import {Jumbotron, Tabs, Tab, Container} from 'react-bootstrap'
 import PortfolioList from '../components/admin/PortfolioList'
 import WelcomeTab from '../components/admin/WelcomeTab'
 
-const Admin = () => {
+import {Redirect} from 'react-router-dom'
+import Userfront from '@userfront/react'
+
+import jwt_decode from 'jwt-decode'
+
+Userfront.init('9ny8dvbd')
+
+
+const Admin = ({location}) => {
+    if (!Userfront.accessToken()) {
+        return (
+            <Redirect to={{
+                pathname: '/login',
+                state: { from: location },
+            }}
+            />
+        )
+    }
+
+    const accessData = jwt_decode(Userfront.accessToken())
+    const userData = jwt_decode(Userfront.idToken())
+
+    console.log("Access", accessData)
+    console.log("user", userData)
+
+    const access = JSON.stringify(accessData)
+    const user = JSON.stringify(userData)
+
     return(
         <Container fluid>
             <Jumbotron>
@@ -18,6 +45,11 @@ const Admin = () => {
                 </Tab>
                 <Tab eventKey="portfolio" title="Portfolio">
                     <PortfolioList />
+                </Tab>
+                <Tab eventKey="user" title="User">
+                     <p>User</p>
+                     <p>{access}</p>
+                     <p>{user}</p>
                 </Tab>
             </Tabs>
         </Container>
